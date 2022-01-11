07b3a4c6a69fde12b7400ec9e81b5690da7ee9b1
Vlad Marchuk <vladmarchuk90@gmail.com>
2022-01-11 15:03:18 +0200

    Validation excersice

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,5 @@ class Product < ApplicationRecord
     message: 'must be a URL for GIF, JPG or PNG image.'
   }
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+  validates :title, length: { minimum: 10 }
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -60,4 +60,14 @@ class ProductTest < ActiveSupport::TestCase
     assert product.invalid?
     assert_equal ["has already been taken"], product.errors[:title]
   end
+
+  test "product is not valid with title less than 10 characters" do
+    product = Product.new(title:        "Short tl",
+                          description: "yyy",
+                          price:        1,
+                          image_url:    "fred.gif")
+
+    assert product.invalid?
+    assert_equal ["is too short (minimum is 10 characters)"], product.errors[:title]
+  end
 end
